34e7bf3a4595905972381e0d243711b2393df338
Maria Eduarda Teodoro <madsfts@gmail.com>
2024-01-26 16:43:14 -0300

    Create basico.js

---
+++ aula 3 (JS)/basico.js
@@ -0,0 +1,23 @@
+// declarando variáveis
+let nome = "Kaia";
+let idade = 25;
+
+// estrutura de controle (if, else)
+if (idade = 10) {
+    console.log(nome + " é maior de idade.");
+} else {
+    console.log (nome + " é menor de idade.");
+}
+
+// loops (for)
+for (let i = 0; i < 5; i++) {
+    console.log("Número: " + 1);
+}
+
+// funções
+function saudacao() {
+    console.log("Olá, " + nome + "!");
+}
+
+// chamando função
+saudacao();
